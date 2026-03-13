cdd4b4946d554d335550a4a17030dbebb6b4433e
HassnainAli <ahassnainsaleem964@gmail.com>
2026-03-13 14:25:49 +0500

    video set

--- src/components/InteractiveVideo.jsx
+++ src/components/InteractiveVideo.jsx
@@ -1,7 +1,7 @@
-// src/components/InteractiveVideo.jsx
 import React from "react";
 import { FaPlayCircle } from "react-icons/fa";
 import { motion } from "framer-motion";
+import videoFile from "../assets/factory hub video.webm";
 
 export default function InteractiveVideo({ onBack }) {
   return (
@@ -23,13 +23,17 @@ export default function InteractiveVideo({ onBack }) {
       </h3>
 
       <div className="max-w-4xl mx-auto bg-white rounded-3xl shadow-2xl overflow-hidden">
-        <video 
-          src="src/assets/factory hub video.webm" 
-          controls 
+        <video
+          src={videoFile}
+          autoPlay
+          muted
+          loop
+          playsInline
           className="w-full rounded-3xl"
-        />
+        />{" "}
         <p className="text-gray-700 mt-6 p-6">
-          Live demonstration videos explaining workflows, features, and step-by-step guidance for users.
+          Live demonstration videos explaining workflows, features, and
+          step-by-step guidance for users.
         </p>
       </div>
     </motion.section>
